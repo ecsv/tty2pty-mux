ccf082e9a0c9dd11a1097ff9da6131576ce5d733
Sven Eckelmann <sven@narfation.org>
2026-05-11 16:00:02 +0200

    contrib: wsterm: switch to to theme with less contrast
    The used color scheme is "Linux Colors" from KDE 25.04.2
    
    Signed-off-by: Sven Eckelmann <sven@narfation.org>

--- contrib/wsterm/src/terminal.js
+++ contrib/wsterm/src/terminal.js
@@ -4,7 +4,29 @@ import { Terminal } from '@xterm/xterm'
 import { FitAddon } from '@xterm/addon-fit'
 
 export function createTerminal(mountEl) {
-  const term = new Terminal({})
+  const term = new Terminal({
+    theme: {
+      background: '#000000',
+      foreground: '#b2b2b2',
+      selectionBackground: 'rgba(57,255,20,0.25)',
+      black: '#000000',
+      brightBlack: '#686868',
+      red: '#b21818',
+      brightRed: '#ff5454',
+      green: '#18b218',
+      brightGreen: '#54ff54',
+      yellow: '#b26818',
+      brightYellow: '#ffff54',
+      blue: '#1818b2',
+      brightBlue: '#5454ff',
+      magenta: '#b218b2',
+      brightMagenta: '#ff54ff',
+      cyan: '#18b2b2',
+      brightCyan: '#54ffff',
+      white: '#b2b2b2',
+      brightWhite: '#55ffff',
+    }
+  })
 
   const fitAddon = new FitAddon()
   term.loadAddon(fitAddon)
